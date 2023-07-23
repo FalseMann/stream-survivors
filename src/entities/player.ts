30b4957c816279dcd7690356ae774e4712373b3a
FalseMann <false@falsemann.com>
2023-07-23 02:34:37 -0700

    feat: change player sprite based on HP

--- src/entities/player.ts
+++ src/entities/player.ts
@@ -5,7 +5,7 @@ export class Player extends Sprite {
 	hpContainer: HpDisplay;
 
 	constructor() {
-		const texture = Texture.from('Cat/Mad.png');
+		const texture = Texture.from('Cat/Smile.png');
 		super(texture);
 		this.anchor.set(0.5);
 		this.hpContainer = new HpDisplay();
@@ -18,5 +18,13 @@ export class Player extends Sprite {
 
 	set hp(hp: number) {
 		this.hpContainer.hp = Math.max(hp, 0);
+
+		if (hp > 80) {
+			this.texture = Texture.from('Cat/Smile.png');
+		} else if (hp > 50) {
+			this.texture = Texture.from('Cat/Weird.png');
+		} else if (hp > 25) {
+			this.texture = Texture.from('Cat/Sad.png');
+		}
 	}
 }
